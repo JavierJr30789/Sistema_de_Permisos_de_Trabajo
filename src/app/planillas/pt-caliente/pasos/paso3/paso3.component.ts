071436eff40d2e081bc2bd1c3726fc0c739a1c29
javierjr30789 <javierjr30789@gmail.com>
2025-02-27 19:41:11 -0300

    mejora de la navegacion entre los pasos para crear la planilla

--- src/app/planillas/pt-caliente/pasos/paso3/paso3.component.ts
+++ src/app/planillas/pt-caliente/pasos/paso3/paso3.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+// Importaciones de PrimeNG
+import { CardModule } from 'primeng/card'; // Para p-card
+import { FieldsetModule } from 'primeng/fieldset'; // Para p-fieldset
+import { InputTextModule } from 'primeng/inputtext'; // Para pInputText
+import { CalendarModule } from 'primeng/calendar'; // Para p-calendar
+import { RadioButtonModule } from 'primeng/radiobutton'; // Para p-radioButton
+import { CheckboxModule } from 'primeng/checkbox'; // Para p-checkbox
+import { ButtonModule } from 'primeng/button'; // Para p-button
+//import { InputTextarea } from 'primeng/inputtextarea';  Para pInputTextarea
+import { FormsModule } from '@angular/forms';
+import { DividerModule } from 'primeng/divider';
+import { DatePickerModule } from 'primeng/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { PanelModule } from 'primeng/panel';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
+
 
 @Component({
   selector: 'app-paso3',
-  imports: [],
+   standalone: true,
+    imports: [FormsModule, CardModule,DatePickerModule,PanelModule, FloatLabelModule ,ReactiveFormsModule,RouterModule, FieldsetModule, InputTextModule, CalendarModule, RadioButtonModule, CheckboxModule, ButtonModule, DividerModule],
   templateUrl: './paso3.component.html',
   styleUrl: './paso3.component.css'
 })
