c1835350ba22a177cb04e90a6bf6cba5b99c6bf6
javierjr30789 <javierjr30789@gmail.com>
2025-03-08 16:24:48 -0300

    segundo paso de la creacion de la planilla digital casi terminada, falta ajustar los botones para cambiar de vista

--- src/app/planillas/pt-caliente/pasos/paso2/paso2.component.ts
+++ src/app/planillas/pt-caliente/pasos/paso2/paso2.component.ts
@@ -15,16 +15,40 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { PanelModule } from 'primeng/panel';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { CommonModule } from '@angular/common';
+import { FileUploadModule } from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @Component({
   selector: 'app-paso2',
    standalone: true,
-    imports: [FormsModule, CardModule,DatePickerModule,PanelModule, FloatLabelModule ,ReactiveFormsModule,RouterModule, FieldsetModule, InputTextModule, CalendarModule, RadioButtonModule, CheckboxModule, ButtonModule, DividerModule],
+    imports: [CommonModule ,FormsModule,HttpClientModule, DividerModule, FileUploadModule, CardModule,DatePickerModule,PanelModule, FloatLabelModule ,ReactiveFormsModule,RouterModule, FieldsetModule, InputTextModule, CalendarModule, RadioButtonModule, CheckboxModule, ButtonModule, DividerModule],
   templateUrl: './paso2.component.html',
   styleUrl: './paso2.component.css'
 })
 export class Paso2Component {
 
+  peligros = [
+    { label: 'Trabajo en altura (Adjuntar Certificado de Trabajo en Altura)', cert: true, file: null, checked: false },
+    { label: 'Entrada en espacio confinado (Adjuntar Certificado de Entrada en Espacio Confinado)', cert: true, file: null, checked: false },
+    { label: 'Aislamiento de energía (Adjuntar Certificado de Aislamiento de Energía)', cert: true, file: null, checked: false },
+    { label: 'Excavaciones (Adjuntar Certificado de Excavación)', cert: true, file: null, checked: false },
+    { label: 'Trabajo con Izaje crítico (Adjuntar Certificado de Izaje)', cert: true, file: null, checked: false },
+    { label: 'Trabajo con electricidad (Adjuntar Certificado de Trabajo Eléctrico y de Aislamiento de Energía)', cert: true, file: null, checked: false },
+    { label: 'Trabajo en red zonas (Autorizados deben firmar este permiso como participantes)', cert: false, checked: false },
+    { label: 'Trabajo crítico (definido en ERO - Evaluación de Riesgo Operacional) Indicar tarea:', cert: false, checked: false },
+    { label: 'Trabajo con anulación (Bypasser) de sistema de seguridad (detallar cuales en el campo observaciones)', cert: false, checked: false }
+  ];
+
+  observaciones: string = '';
+
+  onFileSelect(event: any, item: any) {
+    if (event.files && event.files.length > 0) {
+      item.file = event.files[0]; // ✅ Guardamos el archivo en el objeto correspondiente
+      console.log(`Archivo seleccionado para ${item.label}:`, item.file);
+    }
+  }
+  
 }
